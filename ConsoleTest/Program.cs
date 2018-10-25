89c9081fc6051d016565c5d1aa1059d976e691d9
d.omelchenko <d.omelchenko@apptimized.com>
2018-10-25 15:50:01 +0300

    add helper class

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FFmpegWrapper;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var FfmpegConvertHelper = new FfmpegConvertHelper();
+
+            var canc = new CancellationTokenSource();
+
+          var res = FfmpegConvertHelper.Convert(
+                new []
+                {"D:\\Downloads\\Elementary (Season 05) LostFilm\\Elementary.[S05E01].XviD.LostFilm.[qqss44].avi"},
+                VideoFormat.Same, AudioFormat.Mp2, s => Console.WriteLine(s), canc.Token, null, "Conv");
+            Console.WriteLine(res);
+            var x =  Console.ReadLine();
+
+        }
+    }
+}
---
+++ FFmpegWrapper/FfmpegConvertHelper.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FFmpegWrapper.Extensions;
+
+namespace FFmpegWrapper
+{
+    public enum VideoFormat
+    {
+        [Description("copy")]
+        Same = 0,
+        [Description("avi")]
+        Avi,
+        [Description("mp4")]
+        Mp4
+    }
+
+    public enum AudioFormat
+    {
+        [Description("copy")]
+        Same = 0,
+        [Description("libmp3lame")]
+        Mp3,
+        [Description("mp2")]
+        Mp2,
+    }
+
+    public class FfmpegConvertHelper
+    {
+        private string AddPrefix(string outputPrefix) => string.IsNullOrEmpty(outputPrefix)
+            ? outputPrefix
+            : outputPrefix + "_";
+
+        private  string AddPostfix(string outputPostfix) => string.IsNullOrEmpty(outputPostfix)
+            ? outputPostfix
+            : "_" + outputPostfix;
+
+        private string AddVideoExt(string file, VideoFormat videoFormat)
+        {
+            switch (videoFormat)
+            {
+                case VideoFormat.Same:
+                    return Path.GetExtension(file);
+                case VideoFormat.Avi:
+                    return ".avi";
+                case VideoFormat.Mp4:
+                    return ".mp4";
+            }
+
+            return null;
+        }
+
+
+
+        public bool Convert(IEnumerable<string> inputFiles, VideoFormat videoFormat, 
+            AudioFormat audioFormat, Action<string> writelog, CancellationToken cancellationToken,
+            string destinationFolder = null, string outputPrefix = null, string outPutPostFix = null)
+        {
+            try
+            {
+                var filesArray = inputFiles as string[] ?? inputFiles.ToArray();
+
+                var arguments = new StringBuilder();
+
+                var anotherFolderPlacment = !string.IsNullOrEmpty(destinationFolder);
+
+                foreach (var file in filesArray)
+                {
+                    arguments.Append($"-i \"{file}\" ");
+                }
+
+                for (int i = 0; i < filesArray.Count(); i++)
+                {
+                    var currentFile = filesArray[i];
+
+                    var destinationFileName =
+                        $"{AddPrefix(outputPrefix)}{Path.GetFileNameWithoutExtension(currentFile)}{AddPostfix(outPutPostFix)}{AddVideoExt(currentFile, videoFormat)}";
+
+                    var outPutFile = string.Empty;
+
+                    if (anotherFolderPlacment)
+                        outPutFile = Path.Combine(destinationFolder, destinationFileName);
+                    else
+                        outPutFile = Path.Combine(Path.GetDirectoryName(currentFile), destinationFileName);
+
+                    arguments.Append(
+                        $"-vcodec {videoFormat.GetDescription()} -acodec {audioFormat.GetDescription()} \"{outPutFile}\" ");
+                }
+
+                Console.WriteLine(arguments);
+
+                try
+                {
+                   var ffmpegPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FFmpeg",
+                            "ffmpeg.exe");
+                    Console.WriteLine(ffmpegPath);
+                    var process = new Process()
+                    {
+                        StartInfo = new ProcessStartInfo()
+                        {
+                            FileName = ffmpegPath,
+                            Arguments = arguments.ToString(),
+                            //UseShellExecute = false,
+                            //RedirectStandardOutput = true,
+                            //RedirectStandardError = true
+                        }
+
+                    };
+
+                    //    process.OutputDataReceived += (s, arg) =>
+                    //    {
+                    //        writelog.Invoke(arg.Data);
+                    //    };
+
+                    //    process.ErrorDataReceived += (sender, args) => { writelog?.Invoke(args.Data); };
+
+                    //process.BeginErrorReadLine();
+                    //process.BeginOutputReadLine();
+                    process.Start();
+                    process.WaitForExit();
+                    try
+                    {
+                        //while (!process.HasExited)
+                        //{
+                        //    cancellationToken.ThrowIfCancellationRequested();
+                        //    Thread.Sleep(100);
+                        //}
+
+                        if (process.ExitCode != 0)
+                            return false;
+                    }
+                    catch (OperationCanceledException e)
+                    {
+                        process?.Close();
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    writelog?.Invoke(ex.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                writelog?.Invoke($"Failed with exception {e}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
